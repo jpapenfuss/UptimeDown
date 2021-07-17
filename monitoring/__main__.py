f7ee84411f5718ef23184344d211315ee07c68cb
Rendus <rendus@gmail.com>
2021-07-17 23:28:13 +0000

    Changed output when running module to json
    Signed-off-by: Rendus <rendus@gmail.com>

--- monitoring/__main__.py
+++ monitoring/__main__.py
@@ -9,14 +9,12 @@ def main():
     # See log_setup.py
     logger = log_setup()
     refresh_delay = 1
-
+    import json
     mycpu = cpu.Cpu()
     mymemory = memory.Memory()
     myfs = filesystems.Filesystems()
-    print(mycpu.cpustat_values)
-    print(mycpu.cpuinfo_values)
-    print(mymemory.stats)
-    print(myfs.filesystems)
+
+    print(json.dumps({"cpustats": mycpu.cpustat_values, "cpuinfo": mycpu.cpuinfo_values, "memory": mymemory.stats, "filesystems": myfs.filesystems}, indent=4))
 
 if __name__ == "__main__":
     main()
