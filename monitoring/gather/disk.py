a90cbb9c8471a318e36996564b10ca9f17163c35
Rendus <rendus@gmail.com>
2021-07-21 00:20:01 +0000

    Considerable changes to disk, reading values from new locales

--- monitoring/gather/disk.py
+++ monitoring/gather/disk.py
@@ -1,10 +1,11 @@
-# https://www.kernel.org/doc/Documentation/ABI/testing/procfs-diskstats
-# https://www.kernel.org/doc/Documentation/block/statca.txt
+"""
+https://www.kernel.org/doc/Documentation/ABI/testing/procfs-diskstats
+https://www.kernel.org/doc/Documentation/block/statca.txt
 
-# 259       0 nvme0n1 107146 2852 19971336 23647 181355 11090 15833592 22852 0 66784 92500 13669 0 1758032920 52066
-# 259       1 nvme1n1 109518 3015 19983994 23593 172156 11064 15588608 22670 0 73196 98604 13669 0 1757785272 52757
-#   9     127 md127 222424 0 48560354 0 333649 0 37421048 0 0 0 0 13669 0 3515818192 0
-
+259       0 nvme0n1 107146 2852 19971336 23647 181355 11090 15833592 22852 0 66784 92500 13669 0 1758032920 52066
+259       1 nvme1n1 109518 3015 19983994 23593 172156 11064 15588608 22670 0 73196 98604 13669 0 1757785272 52757
+  9     127 md127 222424 0 48560354 0 333649 0 37421048 0 0 0 0 13669 0 3515818192 0
+"""
 
 # https://www.kernel.org/doc/Documentation/admin-guide/iostats.rst
 
@@ -14,16 +15,15 @@ import os
 import pprint
 import time
 
-pp = pprint.PrettyPrinter(indent=4)
-
 logger = logging.getLogger("monitoring")
-
-# loop = loopback
-# ram = ram
-# nbd = network block device https://www.kernel.org/doc/html/latest/admin-guide/blockdev/nbd.html, we may want to monitor these
-# fbdisk = ?
-# fbsnap = ?
-# zram = Compressed memory as block store https://www.kernel.org/doc/html/latest/admin-guide/blockdev/zram.html
+"""
+loop = loopback
+ram = ram
+nbd = network block device https://www.kernel.org/doc/html/latest/admin-guide/blockdev/nbd.html, we may want to monitor these
+fbdisk = ?
+fbsnap = ?
+zram = Compressed memory as block store https://www.kernel.org/doc/html/latest/admin-guide/blockdev/zram.html
+"""
 
 IGNORE_PREFIXES = [
     "loop",
@@ -82,6 +82,7 @@ class Disk:
     sys_block_path = "/sys/block/"
     # /sys/class/block was not present on my QNAP NAS running kernel 4.14
     sys_class_block_path = "/sys/class/block/"
+    # Directory with device numbers, symlinked to proper subsystems.
     sys_dev_block_path = "/sys/dev/block/"
     # /proc/diskstats has a good chunk of what we need and I wonder if there's anything in /sys/block that wouldn't be in there, or named differently.
     proc_diskstats_path = "/proc/diskstats"
@@ -99,8 +100,10 @@ class Disk:
             diskstats_line = str(reader.readline()).strip().split()
             while diskstats_line != []:
                 if diskstats_line[2].startswith(tuple(IGNORE_PREFIXES)):
+                    # If we're ignoring the device type, read the next line and return to start of loop.
                     diskstats_line = str(reader.readline()).strip().split()
                     continue
+                # If we get the name out of the way, everything else is an int. And we want to index off the name anyway.
                 diskname = diskstats_line.pop(2)
                 diskstats[diskname] = {
                     "iostats": dict(zip(DISKSTAT_KEYS, list(map(int, diskstats_line))))
@@ -110,22 +113,32 @@ class Disk:
                 diskstats_line = str(reader.readline()).strip().split()
         return diskstats
 
-    def get_sys_stats(self, devnum):
-        path = os.path.join(self.sys_dev_block_path, devnum)
-        path = os.scandir(os.path.realpath(path))
-        # Time to figure out what type of directory we have. Options are:
-        # bdi
-        # device (root of block device)
-        # partition
-        # partition with a partition table maybe?
-        # md
-        # dm
-        # nvmeX
-        # nvmeXnY
-        # nvmeXnYpZ
-
-    def get_queue(self, queue):
-        return 0
+    def get_sys_dev_block(self, devnum):
+        # Get the details of one device by device number.
+        path = os.path.realpath(os.path.join(self.sys_dev_block_path, devnum))
+        uevent_path = path + "/uevent"
+        # We'll return dev when it's populated. Create dict here.
+        dev = {}
+        if util.caniread(uevent_path) is False:
+            logger.error(f"Fatal: Can't open {uevent_path} for reading.")
+            return False
+        # Store the resolved path of the block device - This is often a symlink. Always a symlink?
+        # /sys/dev/block/259:0 -> /sys/devices/pci0000:00/0000:00:04.0/nvme/nvme0/nvme0n1
+        dev["realpath"] = path
+        # uevent has the broad details of almost any sysfs directory. This contains:
+        #  MAJOR, MINOR, DEVNAME (nvme0n1p1), DEVTYPE (disk, partition), PARTN (partition number)
+        with open(uevent_path) as reader:
+            # entries are INI-style, NAME=VALUE so split on equal sign.
+            line = str(reader.readline()).strip().split("=")
+            # Empty split will equal [""]
+            while line != [""]:
+                # These are numeric items, so coerce to int
+                if line[0] in ["MAJOR", "MINOR", "PARTN"]:
+                    line[1] = int(line[1])
+                dev[line[0]] = line[1]
+                line = str(reader.readline()).strip().split("=")
+        logger.debug(f"get_sys_dev_block returning {dev} for {devnum}")
+        return dev
 
     def get_disks(self):
         # First let's do the easy thing and get the stats that exist in diskstats:
@@ -136,15 +149,20 @@ class Disk:
                 + ":"
                 + str(devs[dev]["iostats"]["minor"])
             )
-            self.get_sys_stats(devnum)
+            ret = self.get_sys_dev_block(devnum)
+            devs[dev]["path"] = ret["realpath"]
+            devs[dev]["type"] = ret["DEVTYPE"]
+        return devs
 
     def __init__(self):
-        self.get_disks()
+        self.blockdevices = self.get_disks()
 
 
 if __name__ == "__main__":
-    import util # pylint: disable=import-error
+    import util  # pylint: disable=import-error
+
     mydisk = Disk()
+    pp = pprint.PrettyPrinter(indent=4)
     pp.pprint(mydisk.blockdevices)
 else:
     from . import util
