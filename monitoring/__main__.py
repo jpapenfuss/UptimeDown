347db3d684b08ac1eac7c0187dc70d8ed9c0ea15
Rendus <rendus@gmail.com>
2021-07-21 05:23:23 +0000

    Updated __main__ to properly run entire app.

--- monitoring/__main__.py
+++ monitoring/__main__.py
@@ -1,6 +1,10 @@
-#import commands
-from gather import cpu, memory, filesystems
-#import logging
+import time
+
+timestart = time.time()
+# import commands
+from gather import cpu, memory, filesystems, disk
+
+# import logging
 # see log_setup.py
 from log_setup import log_setup
 
@@ -8,13 +12,30 @@ from log_setup import log_setup
 def main():
     # See log_setup.py
     logger = log_setup()
-    refresh_delay = 1
     import json
-    mycpu = cpu.Cpu()
-    mymemory = memory.Memory()
-    myfs = filesystems.Filesystems()
 
-    print(json.dumps({"cpustats": mycpu.cpustat_values, "cpuinfo": mycpu.cpuinfo_values, "memory": mymemory.stats, "filesystems": myfs.filesystems}, indent=4))
+    logger.debug("Gathering CPU stats.")
+    mycpu = cpu.Cpu()
+    logger.debug("Gathering memory stats.")
+    mymemory = memory.Memory()
+    logger.debug("Gathering filesystem stats.")
+    myfs = filesystems.Filesystems()
+    logger.debug("Gathering disk stats.")
+    mydisks = disk.Disk()
+
+    jsonout = json.dumps(
+        {
+            "cpustats": mycpu.cpustat_values,
+            "cpuinfo": mycpu.cpuinfo_values,
+            "memory": mymemory.stats,
+            "filesystems": myfs.filesystems,
+            "disks": mydisks.blockdevices,
+        },
+        indent=4,
+    )
+
+    print(jsonout)
+
 
 if __name__ == "__main__":
     main()
